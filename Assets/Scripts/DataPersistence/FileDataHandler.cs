822b9560cdc4dbf9d5f2d2be3819a7466d51213b
MinhT2000 <manningtran123@gmail.com>
2023-06-07 23:00:23 -0600

    Currently Attempting to do Save and Load System
    If we cannot use the tutorial you can delete the code it should remove the compilation errors

---
+++ Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class FileDataHandler 
+{
+    private string dataDirPath = "";
+
+    private string dataFileName = "";
+
+    private bool useEncryption = false;
+    private readonly string encryptionCodeWord = "word";
+
+    public FileDataHandler(string dataDirPath, string dataFileName, bool useEncryption)
+    {
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
+        this.useEncryption = useEncryption;
+
+    }
+
+    public GameData Load()
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        GameData loadedData = null;
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                //load serialized data from the file
+                string dataToLoad = "";
+                using(FileStream stream = new FileStream(fullPath, FileMode.Open))
+                {
+                    using(StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+
+                //deserialize the data from JSon
+                loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+             {
+            
+                Debug.LogError("Error occured when trying to load data to file: " + fullPath + "\n" + e);
+            }
+        }
+        return loadedData;
+    }
+
+    public void Save(GameData data)
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        try
+        {
+            //create directory file will be written to if it does not exist already
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+            //serialize the c# game data object into JSon
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            
+            Debug.LogError("Error occured when trying to save data to file: " + fullPath + "\n" + e);
+        }
+    }
+
+     private string EncryptDecrypt(string data) 
+    {
+        string modifiedData = "";
+        for (int i = 0; i < data.Length; i++) 
+        {
+            modifiedData += (char) (data[i] ^ encryptionCodeWord[i % encryptionCodeWord.Length]);
+        }
+        return modifiedData;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IDataPersistance
 {
     public static bool IsInDialogue;
 
     public float MoveSpeedX = 5;
     public float MoveSpeedZ = 2;
     
+
+    public void LoadData(GameData data)
+    {
+        this.transform.position = data.playerPosition;
+
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.playerPosition = this.transform.position;
+    }
     // Start is called before the first frame update
     void Start()
     {
